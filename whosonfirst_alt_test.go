a66ec84986976d0a780cf7d1339bd05fec70fcaa
thisisaaronland <aaron@localhost>
2022-05-06 10:58:12 -0700

    add WhosOnFirstAltSPR() method

--- whosonfirst_alt_test.go
+++ whosonfirst_alt_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestWhosOnFirstSPR(t *testing.T) {
+func TestWhosOnFirstAltSPR(t *testing.T) {
 
 	path_fixtures, err := filepath.Abs("fixtures")
 
@@ -16,7 +16,7 @@ func TestWhosOnFirstSPR(t *testing.T) {
 		t.Fatalf("Failed to derive path for fixtures, %v", err)
 	}
 
-	path_feature := filepath.Join(path_fixtures, "101736545.geojson")
+	path_feature := filepath.Join(path_fixtures, "102147495-alt-quattroshapes.geojson")
 
 	r, err := os.Open(path_feature)
 
@@ -32,22 +32,22 @@ func TestWhosOnFirstSPR(t *testing.T) {
 		t.Fatalf("Failed to read %s, %v", path_feature, err)
 	}
 
-	s, err := WhosOnFirstSPR(body)
+	s, err := WhosOnFirstAltSPR(body)
 
 	if err != nil {
 		t.Fatalf("Failed to derive SPR for %s, %v", path_feature, err)
 	}
 
-	if s.Id() != "101736545" {
+	if s.Id() != "102147495" {
 		t.Fatalf("Invalid ID")
 	}
 
-	if s.Name() != "Montreal" {
-		t.Fatalf("Invalid name")
+	if s.Name() != "102147495 alt geometry (quattroshapes)" {
+		t.Fatalf("Invalid name: '%s'", s.Name())
 	}
 
-	if s.Placetype() != "locality" {
-		t.Fatalf("Invalid locality")
+	if s.Placetype() != "alt" {
+		t.Fatalf("Invalid placetype")
 	}
 
 }
