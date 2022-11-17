477b665a9f90f6ec7004d215f4495c68ef0caefd
sfomuseumbot <sfomuseumbot@localhost>
2022-11-17 13:29:00 -0800

    implement follow on sorters for name sorter

--- sort/name.go
+++ sort/name.go
@@ -2,7 +2,7 @@ package sort
 
 import (
 	"context"
-	_ "fmt"
+	"fmt"
 	"github.com/whosonfirst/go-whosonfirst-spr/v2"
 	"sort"
 )
@@ -21,7 +21,7 @@ func NewNameSorter(ctx context.Context, uri string) (Sorter, error) {
 	return s, nil
 }
 
-func (s *NameSorter) Sort(ctx context.Context, results spr.StandardPlacesResults, next ...Sorter) (spr.StandardPlacesResults, error) {
+func (s *NameSorter) Sort(ctx context.Context, results spr.StandardPlacesResults, follow_on_sorters ...Sorter) (spr.StandardPlacesResults, error) {
 
 	lookup := make(map[string][]spr.StandardPlacesResult)
 
@@ -55,8 +55,86 @@ func (s *NameSorter) Sort(ctx context.Context, results spr.StandardPlacesResults
 		}
 	}
 
+	count_follow_on := len(follow_on_sorters)
+
+	if count_follow_on == 0 {
+
+		sorted_results := &SortedStandardPlacesResults{
+			results: sorted,
+		}
+
+		return sorted_results, nil
+	}
+
+	next_sorter := follow_on_sorters[0]
+	var other_sorters []Sorter
+
+	if count_follow_on > 1 {
+		other_sorters = follow_on_sorters[1:]
+	}
+
+	tmp := make(map[string][]spr.StandardPlacesResult)
+	final := make([]spr.StandardPlacesResult, 0)
+
+	last_name := ""
+
+	doNextSort := func(name string) error {
+
+		_results, _ := tmp[name]
+
+		name_results := &SortedStandardPlacesResults{
+			results: _results,
+		}
+
+		name_sorted, err := next_sorter.Sort(ctx, name_results, other_sorters...)
+
+		if err != nil {
+			return fmt.Errorf("Failed to apply next sorter to name '%s', %w", name, err)
+		}
+
+		for _, name_s := range name_sorted.Results() {
+			final = append(final, name_s)
+		}
+
+		return nil
+	}
+
+	for _, s := range sorted {
+
+		name := s.Name()
+
+		if name != last_name {
+
+			if last_name != "" {
+
+				err := doNextSort(last_name)
+
+				if err != nil {
+					return nil, fmt.Errorf("Failed to perform next sort for %s, %w", name, err)
+				}
+			}
+
+			last_name = name
+		}
+
+		_results, ok := tmp[name]
+
+		if !ok {
+			_results = make([]spr.StandardPlacesResult, 0)
+		}
+
+		_results = append(_results, s)
+		tmp[name] = _results
+	}
+
+	err := doNextSort(last_name)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to perform next sort for %s, %w", last_name, err)
+	}
+
 	sorted_results := &SortedStandardPlacesResults{
-		results: sorted,
+		results: final,
 	}
 
 	return sorted_results, nil
